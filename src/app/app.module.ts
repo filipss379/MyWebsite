fce222d66a0c5757e19c868b4a97d6559d3e1e43
Filip Stasiński <filipss379@gmail.com>
2019-10-17 20:44:35 +0200

    Initial files and navbar in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot([
+     { path: 'home', component: HomeComponent },
+     { path: '', redirectTo: 'home', pathMatch: 'full'},
+     { path: '**', redirectTo: 'home', pathMatch: 'full'}
+    ]),
     BrowserModule
   ],
   providers: [],
